d4a9e8113633c91ce55d8357278937eab4dfaccc
MalicknND <ndiayemalick062@gmail.com>
2024-04-17 20:34:14 +0200

    Ajouter la prise en charge de Mongoose pour la connexion à MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import express from "express";
 import data from "./data.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
